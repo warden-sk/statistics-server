f1d0c4490ce9b15ff7f65d79671224bcf2b6d631
Marek Kobida <marek.kobida@gmail.com>
2022-10-27 19:14:18 +0200

    updated

--- test/index.ts
+++ test/index.ts
@@ -8,41 +8,37 @@ import report from '../report';
 
 const WEB_SOCKET_SERVER = 'ws://127.0.0.1:1337';
 
-function $() {
-  report(undefined, '[WebSocket]', WEB_SOCKET_SERVER);
+report(undefined, '[WebSocket]', WEB_SOCKET_SERVER);
 
-  const headers: { [name: string]: string | undefined } = {};
+const headers: { [name: string]: string | undefined } = {};
 
-  const ws = new WebSocket(WEB_SOCKET_SERVER, { headers });
+const ws = new WebSocket(WEB_SOCKET_SERVER, { headers });
 
-  //--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
 
-  ws.on('open', () => {
-    report(undefined, '[WebSocket]', headers);
+ws.on('open', () => {
+  report(undefined, '[WebSocket]', headers);
 
-    const sendCommand = h.sendCommandToServer(ws);
+  const sendCommand = h.sendCommandToServer(ws);
 
-    sendCommand(['MESSAGE', { message: 'Marek Kobida' }]);
-  });
+  sendCommand(['MESSAGE', { message: 'Marek Kobida' }]);
+});
 
-  //--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
 
-  ws.on('upgrade', request => {
-    const SET_COOKIE_HEADER = request.headers['set-cookie'];
+ws.on('upgrade', request => {
+  const SET_COOKIE_HEADER = request.headers['set-cookie'];
 
-    if (Array.isArray(SET_COOKIE_HEADER)) {
-      const FIRST_COOKIE = SET_COOKIE_HEADER[0];
-      const PATTERN = /id=([^;]+)/;
+  if (Array.isArray(SET_COOKIE_HEADER)) {
+    const FIRST_COOKIE = SET_COOKIE_HEADER[0];
+    const PATTERN = /id=([^;]+)/;
 
-      if (FIRST_COOKIE && PATTERN.test(FIRST_COOKIE)) {
-        const [, id] = PATTERN.exec(FIRST_COOKIE) ?? [];
+    if (FIRST_COOKIE && PATTERN.test(FIRST_COOKIE)) {
+      const [, id] = PATTERN.exec(FIRST_COOKIE) ?? [];
 
-        if (id) {
-          headers.cookie = `id=${id}`;
-        }
+      if (id) {
+        headers.cookie = `id=${id}`;
       }
     }
-  });
-}
-
-$();
+  }
+});
