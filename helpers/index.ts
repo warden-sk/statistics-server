b869cb8d91378f1f6ec453699286c28f1161d287
Marek Kobida <marek.kobida@gmail.com>
2022-10-27 03:04:42 +0200

    export all helpers

---
+++ helpers/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2022 Marek Kobida
+ */
+
+export { default as ClientStorage } from './ClientStorage';
+export { default as CookieStorage } from './CookieStorage';
+export { default as FileStorage } from './FileStorage';
+export { default as HistoryStorage } from './HistoryStorage';
+export { default as KnownClientStorage } from './KnownClientStorage';
+export { default as sendCommand } from './sendCommand';
