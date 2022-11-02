5e0b17eb4d863dce98163a446018c50d085b31a7
Marek Kobida <marek.kobida@gmail.com>
2022-11-02 10:13:02 +0100

    update

--- helpers/index.ts
+++ helpers/index.ts
@@ -8,4 +8,6 @@ export { default as FileStorage, FILE_STORAGE_ROW } from './FileStorage';
 export { default as HistoryStorage, HISTORY_STORAGE_ROW } from './HistoryStorage';
 export { default as KnownClientStorage, KNOWN_CLIENT_STORAGE_ROW } from './KnownClientStorage';
 export { default as SubscriberStorage, SUBSCRIBER_STORAGE_ROW } from './SubscriberStorage';
+export { default as keyFromRequest } from './http/keyFromRequest';
 export { default as sendCommand, sendCommandToClient, sendCommandToServer } from './sendCommand';
+export { default as send_json } from './http/send_json';
--- index.ts
+++ index.ts
@@ -3,57 +3,22 @@
  */
 
 import * as h from './helpers';
-import { chainW, isRight, right } from '@warden-sk/validation/Either';
-import { json_decode, json_encode } from '@warden-sk/validation/json';
-import type { Either } from '@warden-sk/validation/Either';
+import { chainW, isRight } from '@warden-sk/validation/Either';
 import commandsFromClient from './commandsFromClient';
 import http from 'http';
+import { json_decode } from '@warden-sk/validation/json';
 import pipe from '@warden-sk/validation/pipe';
-import type stream from 'stream';
 
 const clientStorage = new h.ClientStorage(new h.KnownClientStorage());
 const historyStorage = new h.HistoryStorage();
 const subscriberStorage = new h.SubscriberStorage();
-
-const COOKIE_NAME = 'key';
-
-function keyFromRequest(request: http.IncomingMessage, response: http.ServerResponse): string {
-  const cookieStorage = new h.CookieStorage();
-
-  const cookies = cookieStorage.readCookies(request.headers.cookie ?? '');
-
-  if (h.FileStorage.isValidId(cookies[COOKIE_NAME])) {
-    // cookie exists
-
-    return cookies[COOKIE_NAME];
-  } else {
-    // cookie does not exist
-
-    const key = h.FileStorage.id();
-
-    cookieStorage.writeCookie(COOKIE_NAME, key, { HttpOnly: true });
-
-    response.setHeader('Set-Cookie', cookieStorage.cookies());
-
-    return key;
-  }
-}
-
-function send_json($: stream.Writable): (json: Parameters<typeof json_encode>[0]) => Either<unknown, stream.Writable> {
-  return json =>
-    pipe(
-      json,
-      json_encode,
-      chainW(json => right($.end(json)))
-    );
-}
 
 const server = http.createServer((request, response) => {
   response.setHeader('Access-Control-Allow-Credentials', 'true');
   response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
   response.setHeader('Content-Type', 'application/json');
 
-  const key = keyFromRequest(request, response);
+  const key = h.keyFromRequest(request, response);
 
   /* (1) */ clientStorage.add({ id: key, url: request.url! });
   /* (2) */ const client = clientStorage.row(key)!;
@@ -112,7 +77,7 @@ const server = http.createServer((request, response) => {
     });
   }
 
-  send_json(response)('');
+  h.send_json(response)('');
 });
 
 server.listen(1337);
