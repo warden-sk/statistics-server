64e6ac89fd18f5788ebd89b253283fc843c3c988
Marek Kobida <marek.kobida@gmail.com>
2022-11-02 19:29:56 +0100

    update

---
+++ helpers/messages.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022 Marek Kobida
+ */
+
+const messages = {
+  COMMAND_NOT_VALID: 'The command is not valid.',
+  FILE_NOT_VALID: 'The file is not valid.',
+  REQUEST_NOT_VALID: 'The request is not valid.',
+} as const;
+
+export default messages;
