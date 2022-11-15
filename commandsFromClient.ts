ed8ddcc235bfbec07ebfdafe1405029c7ab24118
Marek Kobida <marek.kobida@gmail.com>
2022-11-15 22:53:55 +0100

    fixed

--- commandsFromClient.ts
+++ commandsFromClient.ts
@@ -5,28 +5,28 @@
 import * as t from '@warden-sk/validation';
 import FileStorage from './helpers/FileStorage';
 
-const MESSAGE_COMMAND = new t.TupleType([
-  new t.LiteralType('MESSAGE'),
-  new t.InterfaceType({
-    message: new t.StringType(), // dokončiť "pattern"
+const MESSAGE_COMMAND = t.tuple([
+  t.literal('MESSAGE'),
+  t.interface({
+    message: t.string(), // dokončiť "pattern"
   }),
 ]);
 
-const SUBSCRIBE_COMMAND = new t.TupleType([
-  new t.LiteralType('SUBSCRIBE'),
-  new t.InterfaceType({
-    'e-mail': new t.StringType(), // dokončiť "pattern"
+const SUBSCRIBE_COMMAND = t.tuple([
+  t.literal('SUBSCRIBE'),
+  t.interface({
+    'e-mail': t.string(), // dokončiť "pattern"
   }),
 ]);
 
-const UPDATE_COMMAND = new t.TupleType([
-  new t.LiteralType('UPDATE'),
-  new t.InterfaceType({
-    url: new t.StringType(), // dokončiť "pattern"
-    windowId: new t.StringType({ pattern: FileStorage.idPattern() }),
+const UPDATE_COMMAND = t.tuple([
+  t.literal('UPDATE'),
+  t.interface({
+    url: t.string(), // dokončiť "pattern"
+    windowId: t.string({ pattern: FileStorage.idPattern() }),
   }),
 ]);
 
-const commandsFromClient = new t.UnionType([MESSAGE_COMMAND, SUBSCRIBE_COMMAND, UPDATE_COMMAND]);
+const commandsFromClient = t.union([MESSAGE_COMMAND, SUBSCRIBE_COMMAND, UPDATE_COMMAND]);
 
 export default commandsFromClient;
--- commandsFromServer.ts
+++ commandsFromServer.ts
@@ -5,33 +5,30 @@
 import * as h from './helpers';
 import * as t from '@warden-sk/validation';
 
-const CLIENT_STORAGE_COMMAND = new t.TupleType([
-  new t.LiteralType('CLIENT_STORAGE'),
-  new t.ArrayType(
-    new t.IntersectionType([
+const CLIENT_STORAGE_COMMAND = t.tuple([
+  t.literal('CLIENT_STORAGE'),
+  t.array(
+    t.intersection([
       h.CLIENT_STORAGE_ROW,
-      new t.InterfaceType({
-        isActive: new t.BooleanType(),
-        isKnown: new t.BooleanType(),
-        name: new t.UnionType([new t.StringType(), new t.UndefinedType()]), // dokončiť
+      t.interface({
+        isActive: t.boolean,
+        isKnown: t.boolean,
+        name: t.union([t.string(), t.undefined]), // dokončiť
       }),
     ])
   ),
 ]);
 
-const HISTORY_STORAGE_COMMAND = new t.TupleType([
-  new t.LiteralType('HISTORY_STORAGE'),
-  new t.ArrayType(h.HISTORY_STORAGE_ROW),
-]);
+const HISTORY_STORAGE_COMMAND = t.tuple([t.literal('HISTORY_STORAGE'), t.array(h.HISTORY_STORAGE_ROW)]);
 
-const MESSAGE_COMMAND = new t.TupleType([
-  new t.LiteralType('MESSAGE'),
-  new t.InterfaceType({
-    createdAt: new t.NumberType(), // dokončiť "pattern"
-    message: new t.StringType(), // dokončiť "pattern"
+const MESSAGE_COMMAND = t.tuple([
+  t.literal('MESSAGE'),
+  t.interface({
+    createdAt: t.number, // dokončiť "pattern"
+    message: t.string(), // dokončiť "pattern"
   }),
 ]);
 
-const commandsFromServer = new t.UnionType([CLIENT_STORAGE_COMMAND, HISTORY_STORAGE_COMMAND, MESSAGE_COMMAND]);
+const commandsFromServer = t.union([CLIENT_STORAGE_COMMAND, HISTORY_STORAGE_COMMAND, MESSAGE_COMMAND]);
 
 export default commandsFromServer;
